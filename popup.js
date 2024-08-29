0dff835a2b605cc05b40acb84db7e06dafd5a06b
anish0714b <swaroopa13132@gmail.com>
2024-08-29 17:51:26 +0530

    changes system

--- popup.js
+++ popup.js
@@ -183,4 +183,85 @@ function checkForSignInButton() {
   }
 }
 
+    
+function bar_createRateBar(likes, dislikes) {
+  let rateBar = document.getElementById("ryd-bar-container");
+  if (!isLikesDisabled()) {
+    // sometimes rate bar is hidden
+    if (rateBar && !isInViewport(rateBar)) {
+      rateBar.remove();
+      rateBar = null;
+    }
+
+    const widthPx =
+      parseFloat(window.getComputedStyle(getLikeButton()).width) +
+      parseFloat(window.getComputedStyle(getDislikeButton()).width) +
+      (isRoundedDesign() ? 0 : 8);
+
+    const widthPercent =
+      likes + dislikes > 0 ? (likes / (likes + dislikes)) * 100 : 50;
+
+    var likePercentage = parseFloat(widthPercent.toFixed(1));
+    const dislikePercentage = (100 - likePercentage).toLocaleString();
+    likePercentage = likePercentage.toLocaleString();
+
+    if (extConfig.showTooltipPercentage) {
+      var tooltipInnerHTML;
+      switch (extConfig.tooltipPercentageMode) {
+        case "dash_dislike":
+          tooltipInnerHTML = `${likes.toLocaleString()}&nbsp;/&nbsp;${dislikes.toLocaleString()}&nbsp;&nbsp;-&nbsp;&nbsp;${dislikePercentage}%`;
+          break;
+        case "both":
+          tooltipInnerHTML = `${likePercentage}%&nbsp;/&nbsp;${dislikePercentage}%`;
+          break;
+        case "only_like":
+          tooltipInnerHTML = `${likePercentage}%`;
+          break;
+        case "only_dislike":
+          tooltipInnerHTML = `${dislikePercentage}%`;
+          break;
+        default: // dash_like
+          tooltipInnerHTML = `${likes.toLocaleString()}&nbsp;/&nbsp;${dislikes.toLocaleString()}&nbsp;&nbsp;-&nbsp;&nbsp;${likePercentage}%`;
+      }
+    } else {
+      tooltipInnerHTML = `${likes.toLocaleString()}&nbsp;/&nbsp;${dislikes.toLocaleString()}`;
+    }
+
+    if (!isShorts()) {
+      if (!rateBar && !isMobile()) {
+        let colorLikeStyle = "";
+        let colorDislikeStyle = "";
+        if (extConfig.coloredBar) {
+          colorLikeStyle = "; background-color: " + getColorFromTheme(true);
+          colorDislikeStyle = "; background-color: " + getColorFromTheme(false);
+        }
+        let actions =
+          isNewDesign() && getButtons().id === "top-level-buttons-computed"
+            ? getButtons()
+            : document.getElementById("menu-container");
+        (
+          actions ||
+          document.querySelector("ytm-slim-video-action-bar-renderer")
+        ).insertAdjacentHTML(
+          "beforeend",
+          `
+              <div class="ryd-tooltip ryd-tooltip-${isNewDesign() ? "new" : "old"}-design" style="width: ${widthPx}px">
+              <div class="ryd-tooltip-bar-container">
+                <div
+                    id="ryd-bar-container"
+                    style="width: 100%; height: 2px;${colorDislikeStyle}"
+                    >
+                    <div
+                      id="ryd-bar"
+                      style="width: ${widthPercent}%; height: 100%${colorLikeStyle}"
+                      ></div>
+                </div>
+              </div>
+              <tp-yt-paper-tooltip position="top" id="ryd-dislike-tooltip" class="style-scope ytd-sentiment-bar-renderer" role="tooltip" tabindex="-1">
+                <!--css-build:shady-->${tooltipInnerHTML}
+              </tp-yt-paper-tooltip>
+              </div>
+      		`,
+        );
+
 })();
